64c793e2d834707ea3479b4b6cf7703f3e500ff4
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-07-13 20:13:09 +0200

    final

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    title: 'dunno',
+    text: "I forgor",
+    username: "Gut",
+    added: new Date(new Date().getTime() + 4*60*60*1000).toLocaleTimeString()
+  },
+  {
+    title: 'dunno',
+    text: "Jou!",
+    user: "Gon",
+    added: new Date(new Date().getTime() + 4*60*60*1000).toLocaleTimeString()
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: "Mini Messageboard", messages: messages })
+});
+
+router.get('/new', function(req, res) {
+  res.render('form')
+})
+
+router.post('/new', (req, res) => {
+  const { title, text, username } = req.body
+
+  const newMessage = {
+    title,
+    text,
+    username,
+    added: new Date(new Date().getTime() + 4*60*60*1000).toLocaleTimeString()
+  };
+  
+  messages.unshift(newMessage);
+  res.redirect('/');
 });
 
 module.exports = router;
